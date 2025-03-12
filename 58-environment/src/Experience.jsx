96a1a2c9bd49fd4f3343abfcaff90c8dcb9677e8
Aurelie <josserand.n.aurelie@gmail.com>
2025-03-12 17:57:14 -0400

    Installs Leva, adds shadows and sky.

--- 58-environment/src/Experience.jsx
+++ 58-environment/src/Experience.jsx
@@ -1,8 +1,9 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls, useHelper } from '@react-three/drei'
+import { OrbitControls, useHelper, BakeShadows, SoftShadows, Sky, useFaceControls} from '@react-three/drei'
 import { useRef } from 'react'
 import { Perf } from 'r3f-perf'
 import * as THREE from 'three'
+import {useControls} from 'leva'
 
 export default function Experience()
 {
@@ -16,7 +17,13 @@ export default function Experience()
         cube.current.rotation.y += delta * 0.2
     })
 
+    const {sunPosition}=useControls('sky',{
+        sunPosition:{value:[1,2,3]}
+    })
+
     return <>
+        {/* <BakeShadows/> */}
+        <SoftShadows size={ 25 } samples={ 10 } focus={ 0 } />
         <color args={['ivory']} attach="background"/>
 
 
@@ -24,20 +31,23 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } ref={directionalLight}/>
+        <directionalLight position={ sunPosition } intensity={ 4.5 } ref={directionalLight} castShadow shadow-mapSize={[1024, 1024]} shadow-camera-top={5} shadow-camera-bottom={-5} shadow-camera-right={5} shadow-camera-left={-5}
+            shadow-camera-near={1} shadow-camera-far={10}/>
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position-x={ - 2 }>
+        <Sky sunPosition={sunPosition}/>
+
+        <mesh position-x={ - 2 } castShadow>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 } castShadow>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 } receiveShadow>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
