ffc0d35401545bdafcd1153ca27073181febaabb
Aurelie <josserand.n.aurelie@gmail.com>
2025-03-11 22:09:27 -0400

    second sync attempt

---
+++ 58-environment/src/Experience.jsx
@@ -0,0 +1,46 @@
+import { useFrame } from '@react-three/fiber'
+import { OrbitControls, useHelper } from '@react-three/drei'
+import { useRef } from 'react'
+import { Perf } from 'r3f-perf'
+import * as THREE from 'three'
+
+export default function Experience()
+{
+    const directionalLight= useRef()
+    useHelper(directionalLight, THREE.DirectionalLightHelper, 1)
+
+    const cube = useRef()
+    
+    useFrame((state, delta) =>
+    {
+        cube.current.rotation.y += delta * 0.2
+    })
+
+    return <>
+        <color args={['ivory']} attach="background"/>
+
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } ref={directionalLight}/>
+        <ambientLight intensity={ 1.5 } />
+
+        <mesh position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+}
