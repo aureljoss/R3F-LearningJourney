0c5a3cf9adaccbd874e156f77de6959fbc2be01e
Aurelie <josserand.n.aurelie@gmail.com>
2025-03-16 12:45:14 -0400

    Adds r3 post processing effects

--- 62-mouse-events/src/Experience.jsx
+++ 62-mouse-events/src/Experience.jsx
@@ -1,9 +1,11 @@
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF, meshBounds } from "@react-three/drei";
 import { useRef } from "react";
 
 export default function Experience() {
   const cube = useRef();
+
+  const hamburger = useGLTF('./hamburger.glb')
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
@@ -30,7 +32,7 @@ export default function Experience() {
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh ref={cube} position-x={2} scale={1.5} onClick={eventHandler} onPointerEnter={()=>{document.body.style.cursor='pointer'}} onPointerLeave={()=>{document.body.style.cursor='default'}}>
+      <mesh raycast={meshBounds} ref={cube} position-x={2} scale={1.5} onClick={eventHandler} onPointerEnter={()=>{document.body.style.cursor='pointer'}} onPointerLeave={()=>{document.body.style.cursor='default' } }>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
@@ -39,6 +41,10 @@ export default function Experience() {
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
+
+      <primitive 
+        object={hamburger.scene} scale={0.25} position-y={0.5} onClick={(event)=>{event.stopPropagation()}}
+      />
     </>
   );
 }
--- 62-mouse-events/src/index.jsx
+++ 62-mouse-events/src/index.jsx
@@ -2,6 +2,7 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import { Bvh } from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -13,7 +14,9 @@ root.render(
             far: 200,
             position: [ - 4, 3, 6 ]
         } }
-    >
-        <Experience />
+    >   
+        <Bvh>
+            <Experience />
+        </Bvh>
     </Canvas>
 )
