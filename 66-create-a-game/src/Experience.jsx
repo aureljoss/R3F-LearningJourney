5a2502b102bc15a2b8851b5e98e122e75555fd14
Aurelie <josserand.n.aurelie@gmail.com>
2025-03-24 12:30:25 -0400

    Adds keyboard controls, adds camera, deletes orbitcontrols

--- 66-create-a-game/src/Experience.jsx
+++ 66-create-a-game/src/Experience.jsx
@@ -1,16 +1,15 @@
-import { OrbitControls } from '@react-three/drei'
 import Lights from './Lights.jsx'
-import Level from './Level.js'
+import {Level} from './Level.js'
 import {Physics} from '@react-three/rapier'
+import Player from './Player.js'
 
 export default function Experience()
 {
     return <>
-
-        <OrbitControls makeDefault />
-        <Physics debug>
+        <Physics debug={false}>
             <Lights />
             <Level/>
+            <Player />
         </Physics>
 
     </>
